3eedeebe56f5f2556d4b76192bc31e0fae692b64
Sebastian Renkonen <100607489+BroccoliBasti@users.noreply.github.com>
2024-01-18 11:38:02 +0200

    fetch data from github

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,31 @@
     <div class="container mx-auto max-w-2xl">
         <header class="flex justify-between items-center mt-5">
             <div>
-                <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Basti</NuxtLink>
+                <NuxtLink to="/" class="text-xl font-semibold p-2 text-red-300">Basti</NuxtLink>
             </div>
             <Menu />
         </header>
-        <main class="p-2">
+        <main class="p-2 mt-10">
             <slot />
         </main>
     </div>
 </template>
+
+<script setup>
+const colorMode = useColorMode();
+console.log(colorMode.preference);
+
+useHead({
+    titleTemplate: '%s - Basti'
+});
+</script>
+
+<style>
+body {
+    @apply bg-white text-gray-700 dark:bg-gray-900 dark:text-gray-300;
+}
+
+li>a {
+    @apply dark:hover:text-gray-900;
+}
+</style>
